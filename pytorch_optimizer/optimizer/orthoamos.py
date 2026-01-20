598a8fdcaf3be40666ccd77b297bfc85a6dd8f8a
James Dean <1125102+JHawkley@users.noreply.github.com>
2026-01-19 21:39:52 -0800

    More debugging.

--- pytorch_optimizer/optimizer/orthoamos.py
+++ pytorch_optimizer/optimizer/orthoamos.py
@@ -1,13 +1,15 @@
 from pytorch_optimizer.optimizer.amos import Amos
 from pytorch_optimizer.optimizer.orthograd import OrthoGrad
+from pytorch_optimizer.base.type import Parameters
 
 class OrthoAmos(OrthoGrad):
     """
     Simple application of OrthoGrad to Amos to make using it in Kohya_ss easier.
     """
 
-    def __init__(self, **kwargs) -> None:
-        super().__init__(Amos, **kwargs)
+    def __init__(self, params: Parameters, lr: float = 1e-3, **kwargs) -> None:
+        optimizer = Amos(params, lr, **kwargs)
+        super().__init__(optimizer)
 
     def __str__(self) -> str:
         return 'OrthoAmos'
